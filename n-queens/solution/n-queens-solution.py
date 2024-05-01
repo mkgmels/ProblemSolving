f359861ebd5c088662a9df28b90074dfdb197154
mkgmels <jb.mlmastery@gmail.com>
2024-05-01 19:24:01 +0300

    solution code modification

--- n-queens/solution/n-queens-solution.py
+++ n-queens/solution/n-queens-solution.py
@@ -1,35 +1,31 @@
 class Solution:
     def solveNQueens(self, n):
-        initial_pos=[n*"." for _ in range(n)]
-        pos_dict={i:initial_pos[i][:i]+"Q"+initial_pos[i][i+1:] for i,val in enumerate(initial_pos)}
-        positions=list(pos_dict.keys())
-        subset,res=[],[]
-        def backtrack(i):
-            #Base cases
-            if len(subset)==n:
-                diff=[subset[j]-j for j in range(n)]
-                d=0
-                while d+1<len(diff):
-                    if diff[d+1]+2==diff[d]:
-                        return
-                    d+=1
-                if len(set(subset))!=len(subset) or len(set(diff))!=len(diff):
-                    return
-                    
-                else:
-                    res.append(subset.copy())
-                    return
+        initial_pos = [n * "." for _ in range(n)]
+        pos_dict = {i: initial_pos[i][:i] + "Q" + initial_pos[i][i + 1:] for i, val in enumerate(initial_pos)}
+        positions = list(pos_dict.keys())
+        subset, res = [], []
 
+        def is_valid(row, col):
+            for i in range(row):
+                if subset[i] == col or abs(subset[i] - col) == row - i:
+                    return False
+            return True
 
+        def backtrack(row):
+            if row == n:
+                res.append(subset[:])
+                return
+            for col in range(n):
+                if is_valid(row, positions[col]):
+                    subset.append(positions[col])
+                    backtrack(row + 1)
+                    subset.pop()
 
-            for i in range(n):
-                subset.append(positions[i])
-                backtrack(i+1)
-                subset.pop()
-           
         backtrack(0)
         for i in range(len(res)):
-            res[i]=[pos_dict[n] for n in res[i]]
-
+            res[i] = [pos_dict[n] for n in res[i]]
 
         return res
+
+
+
